1884739fed3f13d33cc0db1f565e765d85b538d5
Admin <support@unlimitpotential.com>
2023-07-22 18:54:46 -0400

    f

--- apps/server/src/app.controller.ts
+++ apps/server/src/app.controller.ts
@@ -1,12 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('actions') // Specify the base route for this controller
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get() // Handles GET requests to /actions
+  getAction() {
+    // Handle the GET action here
+    return { message: 'This is a GET action' };
+  }
+
+  @Post() // Handles POST requests to /actions
+  postAction(@Body() data: any) {
+    // Handle the POST action here with the incoming request data
+    return { message: 'This is a POST action', data };
   }
 }
