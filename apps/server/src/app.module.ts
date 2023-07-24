abe6cb1e3428d1e40392c9e79548faac76c8c3da
Admin <support@unlimitpotential.com>
2023-07-23 23:34:23 -0400

    j

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { HttpModule } from '@nestjs/axios'; // Import HttpModule from @nestjs/axios
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { TrpcModule } from '@server/trpc/trpc.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), TrpcModule],
+  imports: [ConfigModule.forRoot(), TrpcModule, HttpModule],
   controllers: [AppController],
   providers: [AppService],
 })
