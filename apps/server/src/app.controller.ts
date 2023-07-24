c9777c15e217d1e340bfdd146af5ce80c802e1e6
Admin <support@unlimitpotential.com>
2023-07-23 22:34:34 -0400

    h

--- apps/server/src/app.controller.ts
+++ apps/server/src/app.controller.ts
@@ -4,17 +4,24 @@ import { Controller, Get, Post, Body, Headers } from '@nestjs/common';
 export class AppController {
   private readonly allowedUUID = '22-22-22'; // Replace with your authorized UUID
 
+  private isValidUUID(uuid: string): boolean {
+    // Validate the provided UUID here (e.g., using a library like "uuid")
+    // Return true if the UUID is valid, otherwise return false
+    // Replace the example validation logic below with your actual validation logic
+    return uuid === this.allowedUUID;
+  }
+
   @Get() // Handles GET requests to /actions
   getAction(@Headers('authorization') authorizationHeader: string) {
     const authorizedUUID = authorizationHeader?.split(' ')[1]; // Extract the UUID from the Authorization header
 
     // Check if the provided UUID matches the allowed UUID
-    if (!authorizedUUID || authorizedUUID !== this.allowedUUID) {
-      return { message: 'Unauthorized' };
+    if (!authorizedUUID || !this.isValidUUID(authorizedUUID)) {
+      return { message: 'Unauthorized', isValid: false };
     }
 
-    // If the UUID is valid, respond with "Hello, World!"
-    return { message: 'Hello, World!' };
+    // If the UUID is valid, respond with a JSON object containing the message and isValid value
+    return { message: 'Hello, World!', isValid: true };
   }
 
   @Post() // Handles POST requests to /actions
@@ -25,11 +32,11 @@ export class AppController {
     const authorizedUUID = authorizationHeader?.split(' ')[1]; // Extract the UUID from the Authorization header
 
     // Check if the provided UUID matches the allowed UUID
-    if (!authorizedUUID || authorizedUUID !== this.allowedUUID) {
-      return { message: 'Unauthorized' };
+    if (!authorizedUUID || !this.isValidUUID(authorizedUUID)) {
+      return { message: 'Unauthorized', isValid: false };
     }
 
-    // If the UUID is valid, respond with "Hello, World!"
-    return { message: 'Hello, World!' };
+    // If the UUID is valid, respond with a JSON object containing the message and isValid value
+    return { message: 'Hello, World!', isValid: true };
   }
 }
