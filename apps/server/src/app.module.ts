726213ac0ec81cd900e52ee90ab664220c06477e
Admin <support@unlimitpotential.com>
2023-08-04 22:08:55 -0400

    m

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -1,12 +1,18 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { HttpModule } from '@nestjs/axios'; // Import HttpModule from @nestjs/axios
-import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { HttpModule } from '@nestjs/axios';
 import { TrpcModule } from '@server/trpc/trpc.module';
+import { UploadModule } from './upload/upload.module'; // Import the UploadModule
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
-  imports: [ConfigModule.forRoot(), TrpcModule, HttpModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TrpcModule,
+    HttpModule,
+    UploadModule, // Add the UploadModule here
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
