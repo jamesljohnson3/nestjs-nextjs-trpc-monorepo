3c23a3ba37eb6c57890e8fd5cf1b93e5abe6b8f0
Admin <support@unlimitpotential.com>
2023-08-09 02:24:17 -0400

    r

--- apps/server/src/otp/otp.controller.ts
+++ apps/server/src/otp/otp.controller.ts
@@ -1,28 +1,40 @@
-import { Body, Controller, Post, Req } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { GenerateOtpDto } from './dto/generate-otp.dto';
 import { VerifyOtpDto } from './dto/verify-otp.dto';
-import { OtpService } from './otp.service';
 import { EmailService } from './email.service';
+import * as otpGenerator from 'otp-generator';
+import * as crypto from 'crypto';
 
-@Controller('otp')
-export class OtpController {
-  constructor(
-    private readonly otpService: OtpService,
-    private readonly emailService: EmailService,
-  ) {}
+@Injectable()
+export class OtpService {
+  private otpSecrets: Map<string, string> = new Map(); // Store secrets by user email
 
-  @Post('generate')
-  generateOtp(@Body() generateOtpDto: GenerateOtpDto, @Req() request: any) {
-    const currentUrl = request.headers.referer || ''; // Extract current URL from headers
-    return this.otpService.generateOtp(
-      generateOtpDto,
-      generateOtpDto.email,
-      currentUrl,
-    );
+  constructor(private readonly emailService: EmailService) {}
+
+  generateOtp(
+    generateOtpDto: GenerateOtpDto,
+    email: string,
+    currentUrl: string,
+  ): void {
+    const secret = crypto.randomBytes(16).toString('hex'); // Generate a new random secret
+
+    // Store the secret for the user's email
+    this.otpSecrets.set(generateOtpDto.email, secret);
+
+    // Generate the OTP using otp-generator
+    const otpCode = otpGenerator.generate(6, {
+      digits: true,
+    });
+
+    // Send OTP email
+    this.emailService.sendOtpEmail(generateOtpDto.email, otpCode, currentUrl);
   }
 
-  @Post('verify')
-  verifyOtp(@Body() verifyOtpDto: VerifyOtpDto) {
-    return this.otpService.verifyOtp(verifyOtpDto, verifyOtpDto.email);
+  verifyOtp(verifyOtpDto: VerifyOtpDto, currentUrl: string) {
+    // Always mark OTP verification as successful
+    return {
+      message: 'OTP verification successful (mocked)',
+      isValid: true,
+    };
   }
 }
