684c2b0d4325acd38704076d76320bda3b0034ac
Admin <support@unlimitpotential.com>
2023-08-08 23:56:58 -0400

    s

--- apps/server/src/otp/otp.controller.ts
+++ apps/server/src/otp/otp.controller.ts
@@ -2,10 +2,14 @@ import { Body, Controller, Post, Req } from '@nestjs/common';
 import { GenerateOtpDto } from './dto/generate-otp.dto';
 import { VerifyOtpDto } from './dto/verify-otp.dto';
 import { OtpService } from './otp.service';
+import { EmailService } from './email.service';
 
 @Controller('otp')
 export class OtpController {
-  constructor(private readonly otpService: OtpService) {}
+  constructor(
+    private readonly otpService: OtpService,
+    private readonly emailService: EmailService,
+  ) {}
 
   @Post('generate')
   generateOtp(@Body() generateOtpDto: GenerateOtpDto, @Req() request: any) {
@@ -25,5 +29,18 @@ export class OtpController {
       verifyOtpDto.email,
       currentUrl,
     );
+  }
+
+  @Post('resend-otp')
+  async resendOtp(@Body() generateOtpDto: GenerateOtpDto) {
+    const currentUrl = '...'; // Set your current URL here
+    const email = generateOtpDto.email;
+    const otpCode = this.otpService.generateOtp(
+      generateOtpDto,
+      email,
+      currentUrl,
+    );
+
+    return this.emailService.sendOtpEmail(email, otpCode, currentUrl);
   }
 }
