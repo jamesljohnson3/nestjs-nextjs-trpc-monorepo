fbaf0286d6f904784f992baf04efa3ee1de4fcb0
Admin <support@unlimitpotential.com>
2023-08-09 22:51:59 -0400

    a

--- apps/server/src/check-user/user.controller.ts
+++ apps/server/src/check-user/user.controller.ts
@@ -1,26 +1,36 @@
 import { Controller, Post, Body } from '@nestjs/common';
-import fetch from 'node-fetch';
+import { HttpService } from '@nestjs/axios';
 
-@Controller('check-user')
+@Controller('user')
 export class CheckUserController {
-  @Post('check-user')
-  async sendDataToWebhook(@Body() data: { email: string }) {
+  private readonly webhookUrl =
+    'https://snap-jj3media-icloud-com.eu-1.celonis.cloud/ems-automation/public/api/root/a0e537b1-b88f-434c-a659-0cadea64b085/hook/f03auw3rub1gl5djqehmslc4rpm8j33e';
+
+  constructor(private readonly httpService: HttpService) {}
+
+  @Post('check-existence')
+  async checkUserExistence(
+    @Body() requestBody: { email: string },
+  ): Promise<any> {
     try {
-      const webhookUrl =
-        'Yhttps://snap-jj3media-icloud-com.eu-1.celonis.cloud/ems-automation/public/api/root/a0e537b1-b88f-434c-a659-0cadea64b085/hook/f03auw3rub1gl5djqehmslc4rpm8j33e';
+      console.log('Checking user existence for email:', requestBody.email);
 
-      const response = await fetch(webhookUrl, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(data),
-      });
+      // Create the payload to send to the webhook API
+      const payload = {
+        email: requestBody.email,
+        // Add other fields as needed
+      };
 
-      const webhookResponse = await response.json();
-      return webhookResponse;
+      // Send the POST request to the webhook API
+      const response = await this.httpService
+        .post(this.webhookUrl, payload)
+        .toPromise();
+
+      // Return the data from the webhook API response
+      return response?.data;
     } catch (error) {
-      return { error: 'An error occurred while sending data to webhook.' };
+      console.error('Error communicating with webhook API:', error.message);
+      throw new Error('Error communicating with webhook API');
     }
   }
 }
