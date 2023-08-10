48345b5c30f1f1450873c115152209b3ece8255e
Admin <support@unlimitpotential.com>
2023-08-09 23:08:10 -0400

    a

--- apps/server/src/check-user/user.controller.ts
+++ apps/server/src/check-user/user.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
 
-@Controller('user')
+@Controller('store')
 export class CheckUserController {
   private readonly webhookUrl =
     'https://snap-jj3media-icloud-com.eu-1.celonis.cloud/ems-automation/public/api/root/a0e537b1-b88f-434c-a659-0cadea64b085/hook/f03auw3rub1gl5djqehmslc4rpm8j33e';
