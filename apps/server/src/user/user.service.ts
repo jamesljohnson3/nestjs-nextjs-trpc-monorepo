9495ec77e4727b82de9efc3683fc02333feb4fc0
Admin <support@unlimitpotential.com>
2023-08-09 22:22:00 -0400

    a

--- apps/server/src/user/user.service.ts
+++ apps/server/src/user/user.service.ts
@@ -5,24 +5,27 @@ import { HttpService } from '@nestjs/axios';
 export class UserService {
   constructor(private readonly httpService: HttpService) {}
 
-  async checkUserExistence(email: string, currentUrl: string) {
-    console.log('Checking user existence...');
-    const webhookEndpoint =
-      'https://snap-jj3media-icloud-com.eu-1.celonis.cloud/ems-automation/public/api/root/a0e537b1-b88f-434c-a659-0cadea64b085/hook/f03auw3rub1gl5djqehmslc4rpm8j33e';
-
+  async checkUserExistence(email: string, currentUrl: string): Promise<any> {
     try {
-      console.log('Sending request to webhook API...');
+      console.log('Checking user existence...');
+
+      // Configure your webhook API endpoint
+      const webhookEndpoint =
+        'https://snap-jj3media-icloud-com.eu-1.celonis.cloud/ems-automation/public/api/root/a0e537b1-b88f-434c-a659-0cadea64b085/hook/f03auw3rub1gl5djqehmslc4rpm8j33e';
+
+      // Create the payload to send to the webhook API
+      const payload = {
+        email,
+        currentUrl,
+      };
+
+      // Send the POST request to the webhook API
       const response = await this.httpService
-        .post(webhookEndpoint, { email, currentUrl })
+        .post(webhookEndpoint, payload)
         .toPromise();
 
-      if (response && response.data) {
-        console.log('Webhook response received:', response.data);
-        return response.data;
-      } else {
-        console.error('Invalid response from webhook API');
-        throw new Error('Invalid response from webhook API');
-      }
+      // Return the raw data from the API response
+      return response?.data;
     } catch (error) {
       console.error('Error communicating with webhook API:', error.message);
       throw new Error('Error communicating with webhook API');
