434d8e168e93e9f0835af82a1130d82000de98d3
Admin <support@unlimitpotential.com>
2023-08-09 23:05:32 -0400

    a

--- apps/server/src/check-user/user.controller.ts
+++ apps/server/src/check-user/user.controller.ts
@@ -9,32 +9,15 @@ export class CheckUserController {
   constructor(private readonly httpService: HttpService) {}
 
   @Post('check-existence')
-  async checkUserExistence(
-    @Body() requestBody: { email: string },
-  ): Promise<any> {
+  async checkUserExistence(@Body() data: any): Promise<any> {
     try {
-      console.log('Checking user existence for email:', requestBody.email);
-
-      // Create the payload to send to the webhook API
-      const payload = {
-        email: requestBody.email,
-        // Add other fields as needed
-      };
-
-      console.log('Sending payload to webhook API:', payload);
-
-      // Send the POST request to the webhook API
       const response = await this.httpService
-        .post(this.webhookUrl, payload)
+        .post(this.webhookUrl, data)
         .toPromise();
 
-      console.log('Webhook API response:', response?.data);
-
-      // Return the data from the webhook API response
-      return response?.data;
+      return response!.data; // Use a non-null assertion
     } catch (error) {
-      console.error('Error communicating with webhook API:', error.message);
-      throw new Error('Error communicating with webhook API');
+      throw new Error('Error communicating with webhook endpoint');
     }
   }
 }
