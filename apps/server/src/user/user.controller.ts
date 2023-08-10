722c190b180e68813ad80f164e9514f1e829e616
Admin <support@unlimitpotential.com>
2023-08-09 21:52:17 -0400

    a

--- apps/server/src/user/user.controller.ts
+++ apps/server/src/user/user.controller.ts
@@ -8,7 +8,12 @@ export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post('check-existence')
-  async checkUserExistence(@Body() requestBody: { email: string }) {
-    return this.userService.checkUserExistence(requestBody.email);
+  async checkUserExistence(
+    @Body() requestBody: { email: string; currentUrl: string }, // Provide both email and currentUrl
+  ) {
+    return this.userService.checkUserExistence(
+      requestBody.email,
+      requestBody.currentUrl, // Pass currentUrl as an argument
+    );
   }
 }
--- apps/server/src/user/user.service.ts
+++ apps/server/src/user/user.service.ts
@@ -5,13 +5,13 @@ import { HttpService } from '@nestjs/axios';
 export class UserService {
   constructor(private readonly httpService: HttpService) {}
 
-  async checkUserExistence(email: string) {
+  async checkUserExistence(email: string, currentUrl: string) {
     const webhookEndpoint =
       'https://snap-jj3media-icloud-com.eu-1.celonis.cloud/ems-automation/public/api/root/a0e537b1-b88f-434c-a659-0cadea64b085/hook/f03auw3rub1gl5djqehmslc4rpm8j33e';
 
     try {
       const response = await this.httpService
-        .post(webhookEndpoint, { email })
+        .post(webhookEndpoint, { email, currentUrl })
         .toPromise();
 
       if (response && response.data) {
